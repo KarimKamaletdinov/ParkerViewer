380b0b67490e885b86ec3e05113ef58ad0521dac
karim <karimkamaletdinov55@gmail.com>
2021-02-18 20:03:48 +0500

    ModelDto's content

--- ParkerViewer/Dtos/ModelDto.cs
+++ ParkerViewer/Dtos/ModelDto.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ParkerViewer.Dtos
+﻿namespace ParkerViewer.Dtos
 {
-    class PenDto
+    public class ModelDto
     {
+        public int Id { get; set; }
+        public int CollectionId { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string DetailColor { get; set; }
+        public string WritingType { get; set; }
+        public bool GoldPen { get; set; }
+        public bool Engraving { get; set; }
+        public bool ForMan { get; set; }
+        public bool ForWoman { get; set; }
     }
 }
