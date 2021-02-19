690a0f633f4e0dddccc6f41263fd6d4d83d75683
karim <karimkamaletdinov55@gmail.com>
2021-02-19 21:52:50 +0500

    SqlGetModels' content

--- ParkerViewer/Handlers/Model/SqlGetModels.cs
+++ ParkerViewer/Handlers/Model/SqlGetModels.cs
@@ -1,7 +1,34 @@
-﻿namespace ParkerViewer.Handlers.Model
+﻿using System.Collections.Generic;
+using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Dtos;
+using ParkerViewer.Repositories;
+
+namespace ParkerViewer.Handlers.Model
 {
-    public class SqlGetModels
+    public class SqlGetModels : IQueryHandler<GetModelsQuery, ModelDto[]>
     {
-        
+        public ModelDto[] Execute(GetModelsQuery query)
+        {
+            var models = new List<ModelDto>();
+
+            foreach (var model in new SqlModelRepository().GetAll())
+            {
+               models.Add(new ModelDto()
+               {
+                   Id = model.Id,
+                   CollectionId = model.CollectionId,
+                   DetailColor = model.DetailColor,
+                   Engraving = model.Engraving,
+                   ForMan = model.ForMan,
+                   ForWoman = model.ForWoman,
+                   GoldPen = model.GoldPen,
+                   Name = model.Name,
+                   Price = model.Price,
+                   WritingType = model.WritingType
+               });
+            }
+
+            return models.ToArray();
+        }
     }
 }
