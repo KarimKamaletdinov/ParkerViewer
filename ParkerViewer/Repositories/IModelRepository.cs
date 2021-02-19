0dcb786afc8dafc9928a730002cd8f4dcf4a8d00
karim <karimkamaletdinov55@gmail.com>
2021-02-19 21:27:29 +0500

    IModelRepository's content

--- ParkerViewer/Repositories/IModelRepository.cs
+++ ParkerViewer/Repositories/IModelRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace ParkerViewer.Repositories
+﻿using ParkerViewer.Behaviours;
+
+namespace ParkerViewer.Repositories
 {
     public interface IModelRepository
     {
-        
+        void Insert(Model model);
+
+        void Update(Model model);
+
+        void Delete(Model model);
+
+        Model[] GetAll();
     }
 }
