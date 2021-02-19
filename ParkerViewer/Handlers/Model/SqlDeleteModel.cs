0bcdb5c1115a2b16bf95c8471566095c83d23360
karim <karimkamaletdinov55@gmail.com>
2021-02-19 21:52:24 +0500

    SqlDeleteModel's content

--- ParkerViewer/Handlers/Model/SqlDeleteModel.cs
+++ ParkerViewer/Handlers/Model/SqlDeleteModel.cs
@@ -1,7 +1,26 @@
-﻿namespace ParkerViewer.Handlers.Model
+﻿using ParkerViewer.Abstractions;
+using ParkerViewer.Repositories;
+
+namespace ParkerViewer.Handlers.Model
 {
-    public class SqlDeleteModel
+    public class SqlDeleteModel : ICommandHandler<ModelCommand>
     {
-        
+        public void Execute(ModelCommand command)
+        {
+            new SqlModelRepository().Delete(new Behaviours.Model()
+            {
+                Id = command.ModelDto.Id,
+                CollectionId = command.ModelDto.CollectionId,
+                DetailColor = command.ModelDto.DetailColor,
+                Engraving = command.ModelDto.Engraving,
+                ForMan = command.ModelDto.ForMan,
+                ForWoman = command.ModelDto.ForWoman,
+                GoldPen = command.ModelDto.GoldPen,
+                Name = command.ModelDto.Name,
+                Price = command.ModelDto.Price,
+                WritingType = command.ModelDto.WritingType
+            });
+        }
+
     }
 }
