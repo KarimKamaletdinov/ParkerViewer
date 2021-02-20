ba58a547e9572ceeb98df126470af9826c32cd66
karim <karimkamaletdinov55@gmail.com>
2021-02-20 20:33:40 +0500

    HttpUpdatePen's content

--- ParkerViewer.WebClients/Pen/HttpUpdatePen.cs
+++ ParkerViewer.WebClients/Pen/HttpUpdatePen.cs
@@ -1,7 +1,17 @@
-﻿namespace ParkerViewer.WebClients
+﻿using System.Net;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+
+namespace ParkerViewer.WebClients.Pen
 {
-    public class HttpUpdatePen
+    public class HttpUpdatePen : ICommandHandler<PenCommand>
     {
-        
+        public void Execute(PenCommand command)
+        {
+            new WebClient().UploadString("https://localhost:44380/Pen/" + 
+                command.PenDto.Id, "Put",
+                JsonConvert.SerializeObject(command.PenDto));
+        }
+
     }
 }
