fecf1f300c58b1062eb4bf7cbb6e6f2a86202001
karim <karimkamaletdinov55@gmail.com>
2021-02-20 20:33:52 +0500

    HttpDeletePen's content

--- ParkerViewer.WebClients/Pen/HttpDeletePen.cs
+++ ParkerViewer.WebClients/Pen/HttpDeletePen.cs
@@ -1,7 +1,16 @@
-﻿namespace ParkerViewer.WebClients
+﻿using System.Net;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+
+namespace ParkerViewer.WebClients.Pen
 {
-    public class HttpDeletePen
+    public class HttpDeletePen : ICommandHandler<DeletePenCommand>
     {
-        
+        public void Execute(DeletePenCommand command)
+        {
+            new WebClient().UploadString("https://localhost:44380/Pen/" +
+                command.Id, "Delete","");
+        }
+
     }
 }
