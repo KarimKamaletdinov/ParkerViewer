ed600296ff6d50111d83ed956013a08c7c134a7e
karim <karimkamaletdinov55@gmail.com>
2021-02-20 20:33:27 +0500

    HttpInsertPen's content

--- ParkerViewer.WebClients/Pen/HttpInsertPen.cs
+++ ParkerViewer.WebClients/Pen/HttpInsertPen.cs
@@ -1,7 +1,15 @@
-﻿namespace ParkerViewer.WebClients
+﻿using System.Net;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+
+namespace ParkerViewer.WebClients.Pen
 {
-    public class HttpInsertPen
+    public class HttpInsertPen : ICommandHandler<PenCommand>
     {
-        
+        public void Execute(PenCommand command)
+        {
+            new WebClient().UploadString("https://localhost:44380/Pen", "Post",
+                JsonConvert.SerializeObject(command.PenDto));
+        }
     }
 }
