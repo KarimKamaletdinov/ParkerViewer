d4e7220619da52b990e50a02fc471ecb8cca3437
karim <karimkamaletdinov55@gmail.com>
2021-02-20 20:34:06 +0500

    HttpGetPens' content

--- ParkerViewer.WebClients/Pen/HttpGetPens.cs
+++ ParkerViewer.WebClients/Pen/HttpGetPens.cs
@@ -1,7 +1,17 @@
-﻿namespace ParkerViewer.WebClients
+﻿using System.Net;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Dtos;
+
+namespace ParkerViewer.WebClients.Pen
 {
-    public class HttpGetPens
+    public class HttpGetPens : IQueryHandler<GetPensQuery, PenDto[]>
     {
-        
+        public PenDto[] Execute(GetPensQuery query)
+        {
+            return JsonConvert.DeserializeObject<PenDto[]>(
+                new WebClient().DownloadString("https://localhost:44380/Pen"));
+        }
+
     }
 }
