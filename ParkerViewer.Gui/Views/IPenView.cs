2bdff2c1a138703ddf03267806c8623b4ceadc63
karim <karimkamaletdinov55@gmail.com>
2021-02-21 11:56:13 +0500

    IPenView's content

--- ParkerViewer.Gui/Views/IPenView.cs
+++ ParkerViewer.Gui/Views/IPenView.cs
@@ -1,6 +1,19 @@
-﻿namespace ParkerViewer.Gui.Presenters
+﻿using System;
+using System.Collections.Generic;
+using ParkerViewer.Abstractions.Dtos;
+
+namespace ParkerViewer.Gui.Views
 {
     public interface IPenView
     {
+        event Action<PenDto> InsertPen;
+
+        event Action<PenDto> UpdatePen;
+
+        event Action<int> DeletePen;
+
+        event Action<IPenView> UpdateView;
+
+        List<PenDto> Pens { get; set; }
     }
 }
