10499e06dc9288ee543677ddecabc285f03e0c5a
karim <karimkamaletdinov55@gmail.com>
2021-02-24 20:44:04 +0500

    +PenItemDto

---
+++ ParkerViewer.Abstractions/Dtos/PenItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkerViewer.Abstractions.Dtos
+{
+    public class PenItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ModelId { get; set; }
+        public string Engraving { get; set; }
+        public bool Broken { get; set; }
+    }
+}
