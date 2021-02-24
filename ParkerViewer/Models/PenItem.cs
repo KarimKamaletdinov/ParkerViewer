44ef17e0a566bd83d8249d8b50e7d117cebb644a
karim <karimkamaletdinov55@gmail.com>
2021-02-24 20:44:12 +0500

    +PenItem

---
+++ ParkerViewer/Models/PenItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ParkerViewer.Models
+{
+    public class PenItem
+    {
+        private int _id;
+        private int _modelId;
+        private string _name;
+        private string _engraving;
+        private bool _broken;
+        
+        public int Id
+        {
+            get => _id;
+            set => _id = value <= 0 ? throw new Exception("PenId can not be 0 or " +
+                "negative") : value;
+        }
+
+        public int ModelId
+        {
+            get => _modelId;
+            set => _modelId = value < 0 ? throw new Exception("ModelId can not" +
+                " be negative") : value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value ?? throw new Exception("Name can not" +
+                " be null");
+        }
+        public string Engraving
+        {
+            get => _engraving;
+            set => _engraving = value;
+        }
+        public bool Broken
+        {
+            get => _broken;
+            set => _broken = value;
+        }
+    }
+}
