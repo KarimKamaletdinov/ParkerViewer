9f33c1320ab639712e8120bc437b3384be93975b
karim <karimkamaletdinov55@gmail.com>
2021-02-25 10:30:48 +0500

    +PemItemController

---
+++ ParkerViewer.Host/Controllers/PenItemController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ParkerViewer.Abstractions.Dtos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParkerViewer.Host.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PenItemController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<PenItemDto> Get()
+        {
+            
+        }
+
+        [HttpPost]
+        public void Post([FromBody] PenItemDto value)
+        {
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] PenItemDto value)
+        {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
