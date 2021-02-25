663809e7085a8c2a7437a9994cf160d1beb7113d
karim <karimkamaletdinov55@gmail.com>
2021-02-25 11:00:16 +0500

    +UpdatePenItemWebClient

---
+++ ParkerViewer.WebClients/PenItem/UpdatePenItemWebClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Commands;
+
+namespace ParkerViewer.WebClients.PenItem
+{
+    public class UpdatePenItemWebClient : ICommandHandler<UpdatePenItem>
+    {
+        private readonly string _baseUrl;
+
+        public UpdatePenItemWebClient(string baseUrl = "http://localhost:5000")
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public void Execute(UpdatePenItem command)
+        {
+            var client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            client.Headers.Add("content-type", "application/json");
+            client.UploadString($"{_baseUrl}/PenItem/{command.PenItem.Id}", "PUT",
+                JsonConvert.SerializeObject(command.PenItem));
+        }
+
+    }
+}
