a247fd435e232aa069074b9ed910f5a756e637a3
karim <karimkamaletdinov55@gmail.com>
2021-03-01 12:30:12 +0500

    +UpdateLeadWebClient

---
+++ ParkerViewer.WebClients/Lead/UpdateLeadWebClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Commands.Lead;
+
+namespace ParkerViewer.WebClients.Lead
+{
+    public class UpdateLeadWebClient : ICommandHandler<UpdateLead>
+    {
+        private readonly string _baseUrl;
+
+        public UpdateLeadWebClient(string baseUrl = "http://localhost:5000")
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public void Execute(UpdateLead command)
+        {
+            var client = new WebClient();
+            client.Headers.Add("content-type", "application/json");
+            client.Encoding = Encoding.UTF8;
+            client.UploadString($"{_baseUrl}/Pen/{command.Lead.Id}", "PUT",
+                JsonConvert.SerializeObject(command.Lead));
+        }
+    }
+}
