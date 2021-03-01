899aab259ec5a1dd1d34854883718d6cda369faf
karim <karimkamaletdinov55@gmail.com>
2021-03-01 10:46:16 +0500

    +InsertLeadHandler

---
+++ ParkerViewer/Handlers/Lead/InsertLeadHandler.cs
@@ -0,0 +1,13 @@
+﻿using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Commands.Lead;
+
+namespace ParkerViewer.Handlers.Lead
+{
+    public class InsertLeadHandler : ICommandHandler<InsertLead>
+    {
+        public void Execute(InsertLead command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
