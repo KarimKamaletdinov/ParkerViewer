f8f0d2be987e65d7ddee093299414a9455d6bab0
karim <karimkamaletdinov55@gmail.com>
2021-03-01 10:46:57 +0500

    +GetLeadsHandler

---
+++ ParkerViewer/Handlers/Lead/GetLeadsHandler.cs
@@ -0,0 +1,14 @@
+﻿using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Dtos;
+using ParkerViewer.Abstractions.Queries;
+
+namespace ParkerViewer.Handlers.Lead
+{
+    public class GetLeadsHandler : IQueryHandler<GetLeads, LeadDto[]>
+    {
+        public LeadDto[] Execute(GetLeads command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
