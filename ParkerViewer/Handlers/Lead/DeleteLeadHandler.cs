55fbae81286befbee6268b4cb60f3185de36c9f4
karim <karimkamaletdinov55@gmail.com>
2021-03-01 11:34:22 +0500

    DeleteLeadHandler's content

--- ParkerViewer/Handlers/Lead/DeleteLeadHandler.cs
+++ ParkerViewer/Handlers/Lead/DeleteLeadHandler.cs
@@ -1,13 +1,21 @@
 ﻿using ParkerViewer.Abstractions;
 using ParkerViewer.Abstractions.Commands.Lead;
+using ParkerViewer.Repositories;
 
 namespace ParkerViewer.Handlers.Lead
 {
     public class DeleteLeadHandler : ICommandHandler<DeleteLead>
     {
+        private readonly SqlLeadRepository _repository;
+
+        public DeleteLeadHandler(SqlLeadRepository repository)
+        {
+            _repository = repository;
+        }
+
         public void Execute(DeleteLead command)
         {
-            throw new System.NotImplementedException();
+            _repository.Delete(command.LeadId);
         }
     }
 }
