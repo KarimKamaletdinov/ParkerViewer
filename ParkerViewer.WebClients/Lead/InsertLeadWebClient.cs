601f805b1a89e4ffdf873eb9d2e1bbb4b3979a95
karim <karimkamaletdinov55@gmail.com>
2021-03-01 12:29:57 +0500

    +InsertLeadWebClient

---
+++ ParkerViewer.WebClients/Lead/InsertLeadWebClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Commands.Lead;
+
+namespace ParkerViewer.WebClients.Lead
+{
+    public class InsertLeadWebClient : ICommandHandler<InsertLead>
+    {
+        private readonly string _baseUrl;
+
+        public InsertLeadWebClient(string baseUrl = "http://localhost:5000")
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public void Execute(InsertLead command)
+        {
+            var client = new WebClient();
+            client.Headers.Add("content-type", "application/json");
+            client.Encoding = Encoding.UTF8;
+            client.UploadString($"{_baseUrl}/Pen", "POST",
+                JsonConvert.SerializeObject(command.Lead));
+        }
+    }
+}
