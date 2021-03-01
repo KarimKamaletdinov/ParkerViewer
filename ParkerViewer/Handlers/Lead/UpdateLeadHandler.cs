5caecb33e2c382083a750cde4a4e5d9bb906ba7c
karim <karimkamaletdinov55@gmail.com>
2021-03-01 10:46:30 +0500

    +UpdateLeadHandler

---
+++ ParkerViewer/Handlers/Lead/UpdateLeadHandler.cs
@@ -0,0 +1,13 @@
+﻿using ParkerViewer.Abstractions;
+using ParkerViewer.Abstractions.Commands.Lead;
+
+namespace ParkerViewer.Handlers.Lead
+{
+    public class UpdateLeadHandler : ICommandHandler<UpdateLead>
+    {
+        public void Execute(UpdateLead command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
