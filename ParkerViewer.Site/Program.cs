293fbd1af6371bec70256a38e4f8c885812e77fc
karim <karimkamaletdinov55@gmail.com>
2021-03-02 19:43:46 +0500

    Blazor experiments

--- ParkerViewer.Site/Program.cs
+++ ParkerViewer.Site/Program.cs
@@ -4,19 +4,27 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using ParkerViewer.Abstractions.Dtos;
+using ParkerViewer.Abstractions.Queries;
+using ParkerViewer.WebClients.Pen;
 
 namespace ParkerViewer.Site
 {
     public class Program
     {
+        public static PenDto[] Pens;
+
         public static async Task Main(string[] args)
         {
+            Console.WriteLine("ee");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            builder.Services.AddScoped<WebClient>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
